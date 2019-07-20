44f4d9a98b67d6c415155bfd1b9566e9e98b03ea
Sébastien Besnier <sebastien.fabrice.besnier@gmail.com>
2019-07-20 12:41:55 +0200

    correct db initialization in sh/install.sh

--- sh/install.sh
+++ sh/install.sh
@@ -5,18 +5,20 @@ pip3 install --user --upgrade pip >/dev/null
 pip3 install --user  -r requirements.txt > /dev/null
 echo "Python deps grabbed!"
 
+python3 init_db.py
+
 echo ""
 if [ ! -f "/app/.local/bin/elm" ]; then
    echo "Elm doesn't seem installed, I'm downloading it..."
    cd /app/.local/bin/
-   
+
    curl --silent -L $elm_bin_url  | tar xz
    echo "Elm installed!"
+   cd -
 else
    echo "Elm seems already installed!"
 fi
 
-python3 init_db.py
 
 # experiment for using elm-format but it does not play well with glitch (need to "refresh" after each elm-format...)
 #elmformat_bin_url="https://github.com/avh4/elm-format/releases/download/0.8.1/elm-format-0.8.1-linux-x64.tgz"
@@ -24,7 +26,7 @@ python3 init_db.py
 #if [ ! -f "/app/.local/bin/elm-format" ]; then
 #   echo "Elm-form doesn't seem installed, I'm downloading it..."
 #   cd /app/.local/bin/
-#   
+#
 #   curl --silent -L $elmformat_bin_url  | tar xz
 #   echo "elm-format installed!"
 #else
